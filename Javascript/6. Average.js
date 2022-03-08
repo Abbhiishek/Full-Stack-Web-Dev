af1941d9b107231499092f5ad3027a3b512aae10
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-08 17:05:17 +0530

    Average

---
+++ Javascript/6. Average.js
@@ -0,0 +1,15 @@
+const friendsAge = [20, 19, 22, 21, 23]
+
+const sum = friendsAge[0] + friendsAge[1] + friendsAge[2] + friendsAge[3] + friendsAge[4]
+
+console.log("Sum of friends age", sum)
+
+
+const totalFriends = 5
+
+console.log("total friends", totalFriends)
+
+const average = sum / totalFriends
+
+
+console.log("Average friend age", average)
