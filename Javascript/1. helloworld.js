60a3124826d754dbcff5ab4b8c324c0db6e708c7
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-08 17:04:38 +0530

    hello world

---
+++ Javascript/1. helloworld.js
@@ -0,0 +1,10 @@
+console.log("Hello World")
+
+
+// console log is a function by which you can show some output into the terminal or the console of a web page .
+
+
+// we can aslo output value that are stored in a varibale 
+variable = 100
+
+console.log(variable) // we are taking the value and logging it in the console
